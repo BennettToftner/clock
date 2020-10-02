451da56791f19acc94615e4fca69a551716cb80f
Bennett Toftner <56304614+BennettToftner@users.noreply.github.com>
2020-10-02 09:49:02 -0400

    Add files via upload

---
+++ clock/js/clock.js
@@ -0,0 +1,18 @@
+var canvas = document.getElementById("clock-canvas");
+var ctx = canvas.getContext('2d');
+var DELAY = 20;
+
+var debug = document.getElementById("debug-text");
+
+var d = new Date();
+
+setInterval(updateClock, DELAY);
+
+function updateClock()
+{
+	var ms = d.getTime();
+	var s = 5;
+	debug.innerHTML = ms;
+}
+
+
